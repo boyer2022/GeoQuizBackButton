01a4c1f8475092c76c9e7b66ba04dfbdb589ff2d
matt <emal>
2023-02-05 16:55:21 -0600

    Textbook Chapter 4 “Adding a ViewModel” pg 148-151 of my textbook. Errors unresolved in textbook

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.geoquiz
 
+import QuizViewModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+
+// Adding a TAG constant
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
     // Resource ID's
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var backButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(  // key/value pairs calling on strings.xml
@@ -23,14 +31,23 @@ class MainActivity : AppCompatActivity() {
 
     private var currentIndex = 0    // Index counter
 
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Adding log statement
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+
 
         // Finding Resources by ID's
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        backButton = findViewById(R.id.back_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         // Setting up OnClickListener (What happens when the 'True' button is clicked...)
@@ -47,20 +64,41 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+//            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+
+        backButton.setOnClickListener {
+            quizViewModel.moveToPrevious()
             updateQuestion()
         }
 
         updateQuestion()
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
@@ -69,4 +107,5 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this,messageResId, Toast.LENGTH_SHORT).show()
     }
+
 }
