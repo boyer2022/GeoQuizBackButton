bd1c0981fc6186a17d6ee188da524ae7b31c20f1
matt <emal>
2023-02-19 19:51:36 -0600

    Resolved Errors with QuizViewModel redesigned into java folder. Added back button w/functionality. Unstacked "Next"/"Back" buttons

---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,37 @@
+import androidx.lifecycle.ViewModel
+import com.example.geoquiz.Question
+import com.example.geoquiz.R
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+
+    var currentIndex = 0
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    fun moveToPrevious() {
+        if (currentIndex == 0) {
+            questionBank[currentIndex].textResId
+        } else {
+            currentIndex = (currentIndex - 1) % questionBank.size
+        }
+    }
+
+}
